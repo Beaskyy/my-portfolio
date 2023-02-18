d0bd76ce8e16ac1fd74d028c686245636c50c83c
Beaskyy <afeezakintobi@nqb8.co>
2023-02-18 11:56:21 +0100

    new commit

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,10 +5,10 @@ import { FiDribbble } from "react-icons/fi";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+      <a href="https://www.linkedin.com/in/afeez-akintobi/" target="_blank" rel="noreferrer">
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank" rel="noreferrer">
+      <a href="https://github.com/Beaskyy" target="_blank" rel="noreferrer">
         <FaGithub />
       </a>
       <a href="https://dribbble.com" target="_blank" rel="noreferrer">
